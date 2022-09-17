680102e7dc344d44e0db67bcad4999e3203452f0
Dylan Harrison <dylanharrison@Dylans-Mac-mini.attlocal.net>
2022-09-17 17:13:48 -0500

    deploy commit 2

--- index.js
+++ index.js
@@ -5,6 +5,26 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const path_1 = __importDefault(require("path"));
+const pg_1 = __importDefault(require("pg"));
 const app = (0, express_1.default)();
 app.use(express_1.default.static(path_1.default.join(__dirname, 'client')));
-app.listen(process.env.PORT || 3000, () => console.log(`CrudStore listening on port ${process.env.PORT || 3000}`));
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`CrudStore listening on port ${process.env.PORT || 3000}`);
+    if (process.env.DATABASE_URL) {
+        const pool = new pg_1.default.Pool({
+            connectionString: process.env.DATABASE_URL,
+            ssl: {
+                rejectUnauthorized: false
+            }
+        });
+        if (pool) {
+            console.log(`Successfully connected to Postgres database.`);
+        }
+        else {
+            console.log(`Could not connect to database. DATABASE_URL environtment variable may be set incorrectly.`);
+        }
+    }
+    else {
+        console.log(`Could not connect to database. DATABASE_URL environment variable not set.`);
+    }
+});
